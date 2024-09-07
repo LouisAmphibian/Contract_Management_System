35ead4faf3e2e3872db2d2fb90fd23681f05ae9e
Your Name <ayandamantshinga30@gmail.com>
2024-09-07 09:47:44 +0000

    ADDED FILES

---
+++ TheGym/TheGym/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
