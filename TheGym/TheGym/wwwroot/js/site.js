667d57108af14a0d8f6092fe1a92a52a7e03fd5e
Your Name <ayandamantshinga30@gmail.com>
2024-10-09 10:35:32 +0000

    added function using JavaScript

--- TheGym/TheGym/wwwroot/js/site.js
+++ TheGym/TheGym/wwwroot/js/site.js
@@ -1,4 +1,47 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*my dom variables*/
+const input = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelector(".toggle");
+const main = document.querySelectorAll(".main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
 
-// Write your JavaScript code.
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
+        inp.classLIst.add("active");
+    });
+
+    inp.addEventListener("blur", () => {
+        if (inp.value != "") {
+            return;
+        }
+        inp.classLIst.remove("active");
+    });
+});
+
+toggle_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        main.classLIst.toggle("sign-up-mode");
+    });
+});
+
+//fuction to slide the forms
+function moveSlider() {
+    let index = this.dataset.value;
+
+    let currentImage = document.querySelector('img-${index}');
+
+    images.forEach((img) => img.classList.remove("show"));
+    currentImage.classList.add("show");
+
+    const textSlider = document.querySelector(".text-group");
+
+    textSlider.style.transform = 'translateY(${-(index -1) * 2.2}rem)';
+
+    bullets.forEach((bull) => bull.classList.remove("active"));
+
+    this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+    bullet.addEventListener("click", moveSlider);
+});
