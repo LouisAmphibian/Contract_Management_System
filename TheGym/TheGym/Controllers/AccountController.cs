b84ed1fee89ca0d6ef94169cd022323c0b325657
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-11-22 22:59:50 +0200

    Can Log out and different views are fixed

---
+++ TheGym/TheGym/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheGym.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            //await HttpContext.SignOutAsync(); // Signs out the user
+            return RedirectToAction("Index", "Home"); // Redirects to home or login page
+        }
+    }
+}
